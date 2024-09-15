9877aabc7becfb52ba634c2b7f79d4c00bdf9c65
necit <necitpresents@yandex.ru>
2024-09-15 16:28:59 +0300

    fix bug in clone repo

--- front/src/js/main.js
+++ front/src/js/main.js
@@ -1,5 +1,7 @@
 window.onload = async () => {
 	fetch("http://localhost:4444/is_ok")
 		.then(response => response.json())
-		.then(data => console.log(data.message));
+		.then(data => console.log(data.message))
+		.catch(() => console.warn("DB is not ok"))
+	;
 }
